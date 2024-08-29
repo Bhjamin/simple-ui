2524a7beaf12a8442768a382ecefeda4a49806ce
Ben Hancock <benjamin@hancock1.com>
2024-08-29 09:57:42 -0600

    added prettier formatting

--- src/index.js
+++ src/index.js
@@ -1,8 +1,8 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
+'use strict';
+Object.defineProperty(exports, '__esModule', { value: true });
 exports.add = void 0;
-var utils_js_1 = require("./utils.js");
-Object.defineProperty(exports, "add", {
+var utils_js_1 = require('./utils.js');
+Object.defineProperty(exports, 'add', {
   enumerable: true,
   get: function () {
     return utils_js_1.add;
